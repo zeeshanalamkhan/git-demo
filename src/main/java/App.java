2f347e268d70a329c47a147a4e4654225191bbae
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2018-10-23 16:16:09 +0530

    Added if condition to check the data

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,9 @@ public class App {
 
 	public static void add(String str) {
 
-		System.out.println("Hello " + str);
+		if (str.length() > 5) {
+			System.out.println("Hello " + str);
+		}
 	}
 
 	public static void main(String[] args) {
