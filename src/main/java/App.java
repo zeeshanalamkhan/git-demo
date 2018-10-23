f3520f3fcdf8853ff3a4be7b326b2e91c3469e74
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2018-10-23 16:12:16 +0530

    added git-demo project to local repository

---
+++ src/main/java/App.java
@@ -0,0 +1,15 @@
+
+public class App {
+
+	public static void add(String str) {
+
+		System.out.println("Hello " + str);
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+		add("ZEESHAN");
+	}
+
+}
